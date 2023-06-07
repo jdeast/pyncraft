cd4a53bbe5fb1691fb1dc34b7895196fbe346981
Jason Eastman (windows) <jdeast@gmail.com>
2023-06-07 00:05:14 -0400

    add dependencies

--- setup.py
+++ setup.py
@@ -141,7 +141,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/discussions/install-requires-vs-requirements/
-    install_requires=[],
+    install_requires=["ipdb","stl-to-voxel"],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
